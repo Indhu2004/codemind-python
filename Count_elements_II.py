871ddfc6a6c3fb68d903c77d2b8946ee6323510c
Indhu2004 <96334997+Indhu2004@users.noreply.github.com>
2023-07-05 11:16:21 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) s=list(map(int,input().split())) p=[] for i in l:     if i not in s:         p.append(i) for j in s:     if j not in l:         p.append(j) d=set(p) print(len(d))

---
+++ Count_elements_II.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+s=list(map(int,input().split()))
+p=[]
+for i in l:
+    if i not in s:
+        p.append(i)
+for j in s:
+    if j not in l:
+        p.append(j)
+d=set(p)
+print(len(d))
